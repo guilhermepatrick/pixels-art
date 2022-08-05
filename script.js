5505467a58aeede31f4e942a9b3a17db10bbc165
Guilherme Patrick <guilherme@mgesportes.com.br>
2022-08-05 15:59:33 -0300

    requisito 9 concluido

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let cores = document.getElementsByClassName('color');
 let corSelecionada = document.getElementById('selected-color');
 let pixelsQuadro = document.querySelectorAll('#pixel-board .pixel');
+let botaoLimpar = document.getElementById('clear-board');
 
 // ADICIONANDO EVENT LISTENER EM TODAS AS CORES
-for (let i = 0; i <= cores.length - 1; i += 1) {    
-    cores[i].addEventListener('click', selecionaCor);    
-} 
-// ADICIONANDO EVENT LISTENER EM TODOS PIXELS DO QUADRO  
-for (let i = 0; i <= pixelsQuadro.length - 1; i += 1) {    
-    pixelsQuadro[i].addEventListener('click', colorirPixel)    
+for (let i = 0; i <= cores.length - 1; i += 1) {
+    cores[i].addEventListener('click', selecionaCor);
 }
+// ADICIONANDO EVENT LISTENER EM TODOS PIXELS DO QUADRO  
+for (let i = 0; i <= pixelsQuadro.length - 1; i += 1) {
+    pixelsQuadro[i].addEventListener('click', colorirPixel)
+}
+
+botaoLimpar.addEventListener('click', limpaQuadro)
+
 function selecionaCor() {
-    
+
     corSelecionada.id = event.target.id
-    for(let i = 0; i <= cores.length -1; i++){
+    for (let i = 0; i <= cores.length - 1; i++) {
         console.log('fez o loop')
         cores[i].classList.remove('selected')
     }
@@ -21,7 +25,14 @@ function selecionaCor() {
     console.log('Selecionou a cor')
 }
 
-function colorirPixel(){
+function colorirPixel() {
     event.target.id = corSelecionada.id;
     console.log('colore pixel')
+}
+function limpaQuadro(){
+    for (let i = 0; i <= pixelsQuadro.length - 1; i += 1) {
+        pixelsQuadro[i].id = 'branco'
+    }
+
+
 }
